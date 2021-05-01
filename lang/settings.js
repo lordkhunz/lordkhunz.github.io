1eac8ed158210a1472cd4854fcb37c7f7eef5fa6
lordkhunz <iamkhun@hotmail.co.th>
2021-05-01 22:27:43 +0700

    fucking first commit

---
+++ lang/settings.js
@@ -0,0 +1,42 @@
+export default {
+    default: "en",
+    languages: [
+        // remove the languages you don't need, reorder them, or add your own
+        {
+            locale: "en",
+            name: "English"
+        },
+        {
+            locale: "de",
+            name: "Deutsch"
+        },
+        {
+            locale: "nl",
+            name: "Nederlands"
+        },
+        {
+            locale: "fr",
+            name: "French"
+        },
+        {
+            locale: "fi",
+            name: "Suomi"
+        },
+        {
+            locale: "ja",
+            name: "日本語"
+        },
+        {
+            locale: "ru",
+            name: "Русский"
+        },
+        {
+            locale: "pl",
+            name: "Polski"
+        },
+        {
+            locale: "it",
+            name: "Italiano"
+        },
+    ]
+}
